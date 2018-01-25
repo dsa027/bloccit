2cb5e19d419ee8888132804ddec4837eec8f86f8
David Alderson <dsa027@gmail.com>
2018-01-24 17:17:48 -0700

    Move SPAM updates to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,15 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    Post.all.each_with_index do |post, index|
+      if index % 5 == 0
+        if (post.title != "SPAM")
+          Post.update(post.id, title: "SPAM", body: post.body)
+        end
+      end
+    end
+
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,3 @@
 class Post < ApplicationRecord
   has_many :comments
-
-  Post.all.each_with_index do |post, index|
-    if index % 5 == 0
-      if (post.title != "SPAM")
-        Post.update(post.id, title: "SPAM", body: post.body)
-      end
-    end
-  end
-
 end
