c0cd64b195051f546a548d9feece700dadabb765
David Alderson <dsa027@gmail.com>
2018-01-25 11:38:37 -0700

    Implement Advertisements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
 require_relative 'random_data'
 
-  # Create Posts
-  50.times do
+  def get_title
     title = RandomData.random_sentence
     if title[79]
       title = title.truncate(77) + "..."
     end
+    return title
+  end
+
+  # Create Posts
+  50.times do
     Post.create!(
-      title:  title,
+      title:  get_title,
       body:   RandomData.random_paragraph
     )
   end
@@ -21,27 +25,16 @@ require_relative 'random_data'
     )
   end
 
+  # Create Advertisements
+  100.times do
+    Advertisement.create!(
+      title: get_title,
+      copy: RandomData.random_paragraph,
+      price: sprintf("%02.2f", rand() * 1000)
+    )
+  end
+
   puts "Seed finished"
   puts "#{Post.count} posts created"
   puts "#{Comment.count} comments created"
-
-  # title = "Here is the new titled post"
-  # body = "And here's the body"
-  # cbody = "Comment for newly titled post"
-  #
-  # ret = Post.find_by title: title, body: body
-  # if !ret
-  #   p "New post not found"
-  #   Post.find_or_create_by! title: title, body: body
-  # else
-  #   p "New post found"
-  # end
-  #
-  # ret = Comment.find_by body: cbody
-  # if !ret
-  #   p "New comment not found"
-  #   post = Post.find_by title: title, body: body
-  #   Comment.find_or_create_by! body: cbody, post: post
-  # else
-  #   p "New comment found"
-  # end
+  puts "#{Advertisement.count} advertisements created"
