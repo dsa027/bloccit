d614bd2640ca846394674fac236fc935011d1757
David Alderson <dsa027@gmail.com>
2018-01-25 13:09:19 -0700

    Implement post update and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,14 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
 
-    Post.all.each_with_index do |post, index|
-      if index % 5 == 0
-        if (post.title != "SPAM")
-          Post.update(post.id, title: "SPAM", body: post.body)
-        end
-      end
-    end
+    # this is only making things weird now at cp19
+    # Post.all.each_with_index do |post, index|
+    #   if index % 5 == 0
+    #     if (post.title != "SPAM")
+    #       Post.update(post.id, title: "SPAM", body: post.body)
+    #     end
+    #   end
+    # end
   end
 
   def create
@@ -34,5 +35,32 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "There was an error deleting the post."
+      render :show
+    end
   end
 end
