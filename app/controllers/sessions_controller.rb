abca6422e70d34c4f9bad2a7209500b83577b3f8
David Alderson <dsa027@gmail.com>
2018-01-30 17:22:58 -0700

    Use gravatar to right of signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-
   def new
   end
 
@@ -22,5 +21,4 @@ class SessionsController < ApplicationController
     flash[:notice] = "You've been signed out, come back soon!"
     redirect_to root_path
   end
-
 end
