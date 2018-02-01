a0db5049cd257d8bbb60570992ab321c49c123a7
David Alderson <dsa027@gmail.com>
2018-01-31 18:41:41 -0700

    Authorize moderator to edit and update topics and posts

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,6 +1,9 @@
 module TopicsHelper
-  def user_is_authorized_for_topics?
+  def user_is_authorized_for_topics_crud?
     current_user && current_user.admin?
+  end
+  def user_is_authorized_for_topics_edit?
+    current_user && (current_user.admin? || current_user.moderator?)
   end
   def user_is_signed_in_and_admin?
     current_user && current_user.admin?
